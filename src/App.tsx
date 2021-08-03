ab7e38d58153d08b9135e91dec92ffe2342064ef
Max Frisson <max.chebonenko@gmail.com>
2021-08-03 22:23:36 +0300

    master

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export const App = () => {
+  let [player1Counter, setPlayer1Counter] = useState(10);
+  let [player2Counter, setPlayer2Counter] = useState(10);
+
+  let [counters, setCounters] = useState({
+    c1: 10,
+    c2: 10,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div>
+        <div>Иван Иванович</div>
+        <div>{player1Counter}</div>
+        <button
+          onClick={() => {
+            setPlayer1Counter((actual) => actual + 1);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          +
+        </button>
+      </div>
+      <hr />
+      <div>
+        <div>Пётр Петрович</div>
+        <div>{player2Counter}</div>
+        <button
+          onClick={() => {
+            setPlayer2Counter((actual) => actual + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
+      <hr />
+      <button
+        onClick={() => {
+          setPlayer1Counter((actual) => actual - 1);
+          setPlayer2Counter((actual) => actual - 1);
+        }}
+      >
+        -
+      </button>
+      <button
+        onClick={() => {
+          setPlayer1Counter(10);
+          setPlayer2Counter(10);
+        }}
+      >
+        RESET
+      </button>
     </div>
   );
-}
-
-export default App;
+};
